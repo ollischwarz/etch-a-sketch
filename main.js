c63d1a1a1a269444abe8c8e9877868d0ee66e0be
Oliver Schwarz <oliver.schwarz22@gmail.com>
2024-02-17 10:14:58 +0100

    made the squares in square work

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 let timeout;
 
-window.addEventListener('load', () => {
-    let gridNumber = 16;
-    createGrid(gridNumber);
+window.addEventListener("load", () => {
+  createGrid(64);
 });
 
-window.addEventListener('resize', () => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-        let gridWidth = grid.offsetWidth;
-        console.log('Current grid width:', gridWidth);
-        console.log('Current div width:', gridWidth);
-    }, 50);
+window.addEventListener("resize", () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(createGrid(64), 1000);
 });
 
 let createGrid = (gridNumber) => {
-    for (i = 0; i < (gridNumber ** 2); i++) {
-        let grid = document.querySelector('#grid');
-        let div = document.createElement('div');
-        div.id = `div-${i + 1}`;
-        div.textContent = div.id;
-        div.style.width = (100 / gridNumber) + '%';
-        div.style.height = div.style.width;
-        grid.appendChild(div);
-        console.log(div.id);
-    }
+  let grid = document.querySelector("#grid");
+  grid.style.width = "50vw";
+  grid.style.height = grid.style.width;
+  grid.innerHTML = "";
+
+  for (i = 0; i < gridNumber ** 2; i++) {
+    let div = document.createElement("div");
+    div.id = `${i + 1}`;
+    div.className = `grid-div`;
+    div.style.backgroundColor = getRandomColor();
+    div.style.width = 100 / gridNumber + "%";
+    div.style.height = div.style.width;
+    grid.appendChild(div);
+  }
+};
+
+let getRandomColor = () => {
+  let R = Math.floor(Math.random() * 256);
+  let G = Math.floor(Math.random() * 256);
+  let B = Math.floor(Math.random() * 256);
+  return `rgb(${R},${G},${B})`;
 };
